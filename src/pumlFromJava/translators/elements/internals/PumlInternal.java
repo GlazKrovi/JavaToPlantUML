5d8a67238a4bd6040e6e48e8cfa3da32e6eac298
GlazKrovi <glazvonkrovi@gmail.com>
2023-05-31 17:08:10 +0200

    Add internal element

---
+++ src/pumlFromJava/translators/elements/internals/PumlInternal.java
@@ -0,0 +1,33 @@
+package pumlFromJava.translators.elements.internals;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.type.TypeMirror;
+
+public abstract class PumlInternal implements RawInternal {
+
+    Element self;
+
+    // children have to secure super() with (element == null || element.getKind() != ElementKind.SOMETHING)
+    public PumlInternal(Element self) {
+        this.self = self;
+    }
+
+    @Override
+    public abstract String getContentTranslation() ;
+
+    @Override
+    public String getName() {
+        return self.getSimpleName().toString();
+    }
+
+    @Override
+    public ElementKind getKind() {
+        return self.getKind();
+    }
+
+    @Override
+    public TypeMirror getElementType() {
+        return self.asType();
+    }
+}
---
+++ src/pumlFromJava/translators/elements/internals/RawInternal.java
@@ -0,0 +1,41 @@
+package pumlFromJava.translators.elements.internals;
+
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.type.TypeMirror;
+
+/**
+ * Represents the content of a PumlRawObject,
+ * like method or field
+ */
+public interface RawInternal {
+    /**
+     * Gives the translation of the elements contained in this internal element
+     * (like name and type for a field, or also the returned type for a method)
+     *
+     * @return String like 'fieldName : fieldType' or
+     * 'methodName(arg1Name : arg1Type, arg2Name : arg2Type) : returnedType'
+     */
+    String getContentTranslation();
+
+    /**
+     *
+     * @return Returns the name of the internal element
+     */
+    String getName();
+
+    /**
+     *
+     * @return Returns the type of the internal element,
+     * in an ElementKind form (CLASS, ENUM, INTERFACE, etc.)
+     */
+    ElementKind getKind();
+
+    /**
+     *
+     * @return Returns the entire the name of the element that this internal element
+     * instantiates (like food.Meals, if the internal element is "eat()" method)
+     */
+    TypeMirror getElementType();
+
+
+}
