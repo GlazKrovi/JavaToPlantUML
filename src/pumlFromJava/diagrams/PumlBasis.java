4d68507457fe2cf5592b4e6a9bb303338afdbaf2
GlazKrovi <glazvonkrovi@gmail.com>
2023-06-02 21:55:10 +0200

    Last schemes and rapport's update

--- src/pumlFromJava/diagrams/PumlBasis.java
+++ src/pumlFromJava/diagrams/PumlBasis.java
@@ -8,14 +8,11 @@ import pumlFromJava.translators.marker.Marker;
 
 import javax.lang.model.element.Element;
 
+/**
+ * Translate a specified environment into a very simple Puml scheme
+ * including class, enum and interfaces, without any other info
+ */
 public class PumlBasis implements PumlDiagram {
-    /**
-     * Translate a specified environment into a very simple Puml scheme
-     * including class, enum and interfaces, without any other info
-     *
-     * @param environment a java environment
-     * @return Returns Puml translated String
-     */
     public String translateToScheme(DocletEnvironment environment) {
         Marker marker = new Marker();
         StringBuilder res = new StringBuilder();
