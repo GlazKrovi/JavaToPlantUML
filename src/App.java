b0b190311b5c047f4e25a8d17a6840a4c2cfb645
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2024-03-14 08:51:39 +0100

    fix help unfound error

--- src/App.java
+++ src/App.java
@@ -1,22 +1,19 @@
 import java.util.spi.ToolProvider;
 
-import pumlFromJava.generators.Help;
-
 /*
  * A command line which work:
- * java -cp src/pumlFromJava/generators/Java2Puml.java "-private -sourcepath src -doclet pumlFromJava.doclets.PumlDoclet -d generates/pumls/ -g both -out exemple.puml test"
+ * java -cp src/pumlFromJava/generators/Java2Puml.java -private -sourcepath src -doclet pumlFromJava.doclets.PumlDoclet -d generates/pumls/ -g both -out exemple.puml test"
  *
  * */
 
 public class App {
     public static void main(String[] args) {
         try{
-            // show usages
             ToolProvider toolProvider = ToolProvider.findFirst("javadoc").get();
-            //System.out.println(toolProvider.name());
             toolProvider.run(System.out, System.err, args);
         }
         catch (Exception e){
+            // show usages
             Help help = new Help();
             help.print();
         }
